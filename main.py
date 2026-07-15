4bf7841fc26bb486522f1941d4f0e203f80bc6a9
M-Saad-K <mskscotland@gmail.com>
2026-07-15 20:08:27 +0100

    Writting intial comments

---
+++ main.py
@@ -0,0 +1,27 @@
+
+# Func - Open host folder, let user choose folder
+# Func - check each files, one by one
+# Func file exten, activate func for extension
+# Func - If file for _____, if they don't exist, take file as 
+# Func move given file into folder
+"""
+images
+.png
+.jpg
+.webp
+
+documents
+.pdf
+.docx
+.txt
+
+coding
+.py
+.java
+.c
+
+videos
+.mp4
+.mp3
+
+"""
