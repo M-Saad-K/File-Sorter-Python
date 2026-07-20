161e41f2e8c43101aeed9c48982046ceb579e9a9
M-Saad-K <mskscotland@gmail.com>
2026-07-20 20:19:09 +0100

    Beginning planning

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-
+import sys
 # Func - Open host folder, let user choose folder
 # Func - check each files, one by one
 # Func file exten, activate func for extension
-# Func - If file for _____, if they don't exist, take file as 
+# Func - If folder for _____, if they don't exist, take file as 
 # Func move given file into folder
 """
 images
@@ -25,3 +25,20 @@ videos
 .mp3
 
 """
+
+def main():
+
+    # assig_folder = openFolder()
+
+    return
+
+def openFolder():
+    # Take user input
+    # Open folder
+    # return that the folder has opened and are entered
+
+def 
+
+
+if __name__ == '__main__' :
+    main() 
